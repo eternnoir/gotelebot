d55412244a8d2c8fa556819014541fedc94bb0fa
eternnoir <eternnoir@gmail.com>
2015-08-27 11:30:59 +0800

    Add StartPolling method support.

--- telebot.go
+++ telebot.go
@@ -1,25 +1,29 @@
 package gotelebot
 
 import (
+	"errors"
+	"fmt"
 	"github.com/eternnoir/gotelebot/types"
+	"strconv"
 )
 
 type Bot interface {
-	GetMe() *types.User
-	GetUpdate() []*types.Message
-	StartPolling()
+	GetMe() (*types.User, error)
+	GetUpdate() ([]*types.Message, error)
+	StartPolling() error
 }
 
 type TeleBot struct {
 	token    string
-	Messages *chan (string)
+	Messages chan (*types.Message)
+	Offset   float64
 }
 
 func InitTeleBot(botToken string) *TeleBot {
 	bot := new(TeleBot)
 	bot.token = botToken
-	ch := make(chan string)
-	bot.Messages = &ch
+	ch := make(chan *types.Message)
+	bot.Messages = ch
 	return bot
 }
 
@@ -29,4 +33,43 @@ func (bot *TeleBot) GetMe() (*types.User, error) {
 
 func (bot *TeleBot) GetUpdates(offset, limit, timeout string) ([]*types.Update, error) {
 	return getUpdates(bot.token, offset, limit, timeout)
+}
+
+func (bot *TeleBot) StartPolling(nonStop bool) error {
+	for {
+		fmt.Println(int(bot.Offset))
+		newUpdates, err := bot.GetUpdates(strconv.Itoa(int(bot.Offset)), "", "")
+		if err != nil {
+			if !nonStop {
+				return err
+			} else {
+				fmt.Println(err)
+			}
+		}
+		newMessages, errs := bot.processNewUpdate(newUpdates)
+		if errs != nil {
+			if !nonStop {
+				return errs
+			} else {
+				fmt.Println(errs)
+			}
+		}
+		for _, m := range newMessages {
+			bot.Messages <- m
+		}
+	}
+}
+
+func (bot *TeleBot) processNewUpdate(updates []*types.Update) ([]*types.Message, error) {
+	retMessages := []*types.Message{}
+	for _, update := range updates {
+		if update.UpdateId >= bot.Offset {
+			bot.Offset = update.UpdateId + 1
+		}
+		if update.Message == nil {
+			return nil, errors.New("[telebot][ERROR] Message is null.")
+		}
+		retMessages = append(retMessages, update.Message)
+	}
+	return retMessages, nil
 }
